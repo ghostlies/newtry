0f566889517bad96b9a0ac7c5b782140fd708ae3
ghostlies <97218292+ghostlies@users.noreply.github.com>
2024-04-30 18:32:13 +0200

    Create login_view.inc.php

---
+++ includes/login_view.inc.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types= 1);
+
+function check_login_errors() {
+    if (isset($_SESSION["errors_login"])) {
+        $errors = $_SESSION['errors_login'];
+        foreach ($errors as $error) {
+            echo '<p class="form-error">' . $error . '</p>';
+        }
+        unset($_SESSION['errors_login']);
+    } elseif (isset($_GET['login']) && $_GET['login'] === 'success') {
+        echo '<p class="form-success">Login successful!</p>';
+    }
+}
+
